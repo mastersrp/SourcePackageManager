9e5a564dcb758836d7ba2a8de47eca2b9607bd11
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-03-05 16:57:01 +0000

    Change bootstrap script to use REPO variable instead of fully hardcoded constant. Add paklib/hello_world.c to showcase a simple tcc -run file. Update scripts/Tupfile to depend on the tcc binary too. Update scripts/addpayload.sh to also include the tcc native binary. Update src/spm.c to call lua, further calling tcc.

--- src/spm.c
+++ src/spm.c
@@ -3,12 +3,15 @@
 #include "script.h"
 
 const char *hello_from_lua = "io.output():write('Hello world, lua calling!\\n')";
+const char *tcc_run = "os.execute('tcc -run .spm/paklib/hello_world.c')";
 
 int main( int argc, char **argv ) {
 	printf( "Hello world\n" );
 	script_init();
 	script_openlibs();
 	script_dostring( hello_from_lua );
+	printf( "Attempting to test tcc calls from lua...\n");
+	script_dostring( tcc_run );
 	script_deinit();
 	return 0;
 }
