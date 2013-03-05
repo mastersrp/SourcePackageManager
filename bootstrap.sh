26fe5226e430c950b80e55d6a260a48b74b227c0
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-03-05 14:45:12 +0000

    Fix .gitignore not ignoring scripts/spm.sh bootstrap.sh: 	Stop exporting new PATH variables. 	Fetch ${2}.build repository first, for future compability with 	dependency testing. 	Output git cloning to .spm/log AND stdout with tee .spm/log -a Add doc/chapter1.txt for future article on the package manager. Add doc/preface.txt to give readers an idea on what and why I'm doing this. Add required payload scripts and setup to scripts/

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
-cd .;
-export PATH=$PWD:$PATH
+mkdir -p .spm/
 
 # Parameters for getpack are (category)/(package) (base_package_name) (tag/branch)
 # Note that remote tags can be fetched running
 # `git ls-remote --tags (remote_url)`
 function getpack {
-	git clone file://${HOME}/work/tuprepo/${1} deps/${2} --single-branch --branch ${3}
-	git clone file://${HOME}/work/tuprepo/${1}.build deps/${2}.build --single-branch --branch ${3}
-	cp -Rf deps/${2}.build/* deps/${2}
-	rm -Rf deps/${2}.build/
+	git clone file://${HOME}/work/tuprepo/${1}.build deps/${2}.build --single-branch --branch ${3} | tee .spm/log -a;
+	git clone file://${HOME}/work/tuprepo/${1} deps/${2} --single-branch --branch ${3} | tee .spm/log -a;
+	cp -Rf deps/${2}.build/* deps/${2};
+	rm -Rf deps/${2}.build/;
 }
 if [ ! -d "deps/lua" ]; then
-	printf "Fetching dev-lang/lua-5.2.1 from repos...\n";
-	getpack dev-lang/lua lua v5.2.1
+	printf "Fetching dev-lang/lua-5.2.1 from repos...\n" | tee .spm/log -a;
+	getpack dev-lang/lua lua v5.2.1;
 fi
 if [ ! -d "deps/tinycc" ]; then
-	printf "Fetching dev-lang/tinycc from repos...\n";
-	getpack dev-lang/tinycc tinycc release_0_9_26
+	printf "Fetching dev-lang/tinycc from repos...\n" | tee .spm/log -a;
+	getpack dev-lang/tinycc tinycc release_0_9_26;
 fi
 TUP=$(which tup);
 if [ ! -e $TUP ]; then
-	printf "Building tup...\n";
-	git clone git://github.com/gittup/tup.git deps/tup
+	printf "Building tup...\n" | tee .spm/log -a;
+	git clone git://github.com/gittup/tup.git deps/tup | tee .spm/log -a
 	cd deps/tup;
-	./bootstrap.sh
+	./bootstrap.sh;
 	cp -Rf {.tup,tup} ../../
 	cd ../..;
-	TUP=${PWD}/tup
-	rm -Rf 
+	TUP=${PWD}/tup;
+	rm -Rf;
 fi
 if [ ! -d ".tup" ]; then
-	$TUP init
+	$TUP init;
 fi
 $TUP upd;
