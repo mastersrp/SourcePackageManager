d9dee0996de9c3ac8e06e92b46a597bdf3b76379
T3hGamerDK <tcg.thegamer@gmail.com>
2013-03-05 17:57:01 +0100

    Change bootstrap script to use REPO variable instead of fully hardcoded constant. Add paklib/hello_world.c to showcase a simple tcc -run file. Update scripts/Tupfile to depend on the tcc binary too. Update scripts/addpayload.sh to also include the tcc native binary. Update src/spm.c to call lua, further calling tcc.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 mkdir -p .spm/
+REPO=file://${HOME}/work/tuprepo
 
 # Parameters for getpack are (category)/(package) (base_package_name) (tag/branch)
 # Note that remote tags can be fetched running
 # `git ls-remote --tags (remote_url)`
 function getpack {
-	git clone file://${HOME}/work/tuprepo/${1}.build deps/${2}.build --single-branch --branch ${3} | tee .spm/log -a;
-	git clone file://${HOME}/work/tuprepo/${1} deps/${2} --single-branch --branch ${3} | tee .spm/log -a;
+	git clone ${REPO}/${1}.build deps/${2}.build --single-branch --branch ${3} | tee .spm/log -a;
+	git clone ${REPO}/${1} deps/${2} --single-branch --branch ${3} | tee .spm/log -a;
 	cp -Rf deps/${2}.build/* deps/${2};
 	rm -Rf deps/${2}.build/;
 }
 if [ ! -d "deps/lua" ]; then
-	printf "Fetching dev-lang/lua-5.2.1 from repos...\n" | tee .spm/log -a;
+	printf "Fetching dev-lang/lua v5.2.1 from repos...\n" | tee .spm/log -a;
 	getpack dev-lang/lua lua v5.2.1;
 fi
 if [ ! -d "deps/tinycc" ]; then
-	printf "Fetching dev-lang/tinycc from repos...\n" | tee .spm/log -a;
+	printf "Fetching dev-lang/tinycc release_0_9_26 from repos...\n" | tee .spm/log -a;
 	getpack dev-lang/tinycc tinycc release_0_9_26;
+fi
+if [ ! -d "deps/tup" ]; then
+	printf "Fetching dev-util/tup v0.6 from upstream...\n" | tee .spm/log -a;
+	git clone git://github.com/gittup/tup.git deps/tup --single-branch --branch v0.6 | tee .spm/log -a
 fi
 TUP=$(which tup);
 if [ ! -e $TUP ]; then
 	printf "Building tup...\n" | tee .spm/log -a;
-	git clone git://github.com/gittup/tup.git deps/tup | tee .spm/log -a
 	cd deps/tup;
 	./bootstrap.sh;
 	cp -Rf {.tup,tup} ../../
--- scripts/addpayload.sh
+++ scripts/addpayload.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Ensure that .spm/ exists before trying to write to it.
 mkdir -p .spm/
 cp -f ../src/spm .spm/spm
+cp -f ../deps/tinycc/tcc .spm/tcc
+cp -f ../deps/tup/tup .spm/tup
 cp -Rf ../paklib/ .spm/paklib/
 tar -czf spm.tgz .spm/
 cp -f spm.sh.in spm.sh
