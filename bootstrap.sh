802aa176b7f2a888aa83b0c1660fc18593027761
T3hGamerDK <tcg.thegamer@gmail.com>
2013-03-06 16:54:07 +0100

    Comment out musl dependency. Not currently building.

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,23 +16,29 @@ if [ ! -d "deps/lua" ]; then
 	printf "Fetching dev-lang/lua v5.2.1 from repos...\n" | tee .spm/log -a;
 	getpack dev-lang/lua lua v5.2.1;
 fi
+
 if [ ! -d "deps/tinycc" ]; then
 	printf "Fetching dev-lang/tinycc release_0_9_26 from repos...\n" | tee .spm/log -a;
 	getpack dev-lang/tinycc tinycc release_0_9_26;
 fi
+
 if [ ! -d "deps/tup" ]; then
 	printf "Fetching dev-util/tup v0.6 from upstream...\n" | tee .spm/log -a;
 	git clone git://github.com/gittup/tup.git deps/tup --single-branch --branch v0.6 | tee .spm/log -a;
 fi
-if [ ! -d "deps/musl" ]; then
-	printf "Fetching sys-libs/musl v0.9.9 from repos...\n" | tee .spm/log -a;
-	getpack sys-libs/musl musl v0.9.9;
-fi
+
+# Commented out for internal testing and review
+#if [ ! -d "deps/musl" ]; then
+#	printf "Fetching sys-libs/musl v0.9.9 from repos...\n" | tee .spm/log -a;
+#	getpack sys-libs/musl musl v0.9.9;
+#fi
+
 # Commented out for internal testing and review
 #if [ ! -d "deps/libgit2" ]; then
 #	printf "Fetching dev-libs/libgit2 v0.17.0 from upstream...\n" | tee .spm/log -a;
 #	getpack dev-libs/libgit2 libgit2 v0.17.0
 #fi
+
 TUP=$(which tup);
 if [ ! -e $TUP ]; then
 	printf "Building tup...\n" | tee .spm/log -a;
