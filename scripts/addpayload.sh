439440431c7cdfd56fced56a01f6c737c71b9fd8
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-03-08 13:04:33 +0000

    Fix checking for wrong directories when extracting binary payload. Fix building libspm.a with script.o, now uses build.o. spm is now build directly with script.o, since the package manager library itself does not rely on scripting capabilities.

--- scripts/addpayload.sh
+++ scripts/addpayload.sh
@@ -2,13 +2,14 @@
 
 # Ensure that .spm/ exists before trying to write to it.
 mkdir -p .spm/paklib/include/spm
-cp -f ../src/spm .spm/spm
+mkdir -p .spm/bin
+cp -f ../src/spm .spm/bin/spm
 cp -f ../src/libspm.a .spm/paklib/
-cp -f ../deps/tinycc/tcc .spm/tcc
+cp -f ../deps/tinycc/tcc .spm/bin/tcc
 cp -f ../deps/tinycc/lib/libtcc1.a .spm/paklib/include/
 cp -Rf ../deps/tinycc/include .spm/paklib/
 cp -Rf ../src/*.h .spm/paklib/include/spm/
-cp -f ../deps/tup/tup .spm/tup
+cp -f ../deps/tup/tup .spm/bin/tup
 cp -Rf ../paklib/ .spm/
 tar -czf spm.tgz .spm/
 cp -f spm.sh.in spm.sh
