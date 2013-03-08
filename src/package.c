86c88a7d7509e0353102463a14bbcb94342efb45
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-03-08 12:53:23 +0000

    Add scripting api possibilities for tcc-compiled scripts.

--- src/package.c
+++ src/package.c
@@ -1,10 +1,9 @@
 #include "package.h"
-#include "script.h"
 
-int spm_package_retrieve( struct package_t *package ) {
+int spm_package_retrieve( struct spm_package_t *package ) {
 	return 0;
 }
 
-int spm_package_configure( struct package_t *package ) {
+int spm_package_configure( struct spm_package_t *package ) {
 	return 0;
 }
