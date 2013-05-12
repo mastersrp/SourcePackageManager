978f04c45ab090c88c789fa049367964269cd5df
mastersrp <tcg.thegamer@gmail.com>
2013-05-13 00:03:35 +0200

    Add entire build/ directory to gitignore. tup.config is now generated. Extend Sourcefile to, temporarily, allow custom configure commands to ease building of non-tup enabled software. Add vim syntax to Sourcefile for better readability. Remove getpack function bootstrap. Now using scripts/getdeps.lua instead. Note that this requires Lua to be installed. Change dependency cloning to actually use Sourcefile with scripts/getdeps.lua. Update Tupfile to use @(DIR_LUA_ROOT) to access the root directory of dependency with name 'lua'. This works for all dependencies. Remove traces of TinyCC in Tuprules.tup.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 CWD=$(pwd -P)
-mkdir -p ${CWD}/.spm/
-REPO=file://${HOME}/work/tuprepo
-if [[ ! -d ${REPO:7} ]]; then
-	REPO="http://dl.dropbox.com/u/5579836/tuprepo"
+install -d ${CWD}/.spm/
+install -d ${CWD}/build
+
+which lua &>/dev/null
+lua_installed=$?
+
+if [[ "$lua_installed" == "0" ]]; then
+	printf "" > build/tup.config
+	lua scripts/getdeps.lua
+	[[ ! "$?" == "0" ]] && exit 1
+else
+	printf "[!] You'll need lua installed to compile this project!\n"
+	exit 1
 fi
 
-# Parameters for getpack are (category)/(package) (base_package_name) (tag/branch)
-# Note that remote tags can be fetched running
-# `git ls-remote --tags (remote_url)`
-function getpack {
-	git clone ${REPO}/${1}.build ${CWD}/deps/${2}.build --single-branch --branch ${3} | tee .spm/log -a;
-	git clone ${REPO}/${1} ${CWD}/deps/${2} --single-branch --branch ${3} | tee .spm/log -a;
-	cp -Rf ${CWD}/deps/${2}.build/* ${CWD}/deps/${2};
-	rm -Rf ${CWD}/deps/${2}.build/;
-}
-
-if [ ! -d "deps/lua" ]; then
-	printf "Fetching dev-lang/lua v5.2.1 from repos...\n" | tee .spm/log -a;
-	getpack dev-lang/lua lua v5.2.1;
+if [[ ! -s "build/tup.config" ]]; then
+	echo "CONFIG_DEBUG=n" >> build/tup.config
+	echo "CONFIG_CC=gcc" >> build/tup.config
+	echo "CONFIG_AR=ar rcu" >> build/tup.config
 fi
-
-if [ ! -d "deps/tup" ]; then
-	printf "Fetching dev-util/tup v0.6 from upstream...\n" | tee .spm/log -a;
-	git clone git://github.com/gittup/tup.git deps/tup --single-branch --branch v0.6 | tee .spm/log -a;
-fi
-
-# Commented out for internal testing and review
-#if [ ! -d "deps/musl" ]; then
-#	printf "Fetching sys-libs/musl v0.9.9 from repos...\n" | tee .spm/log -a;
-#	getpack sys-libs/musl musl v0.9.9;
-#fi
-
-# Commented out for internal testing and review
-#if [ ! -d "deps/libgit2" ]; then
-#	printf "Fetching dev-libs/libgit2 v0.17.0 from upstream...\n" | tee .spm/log -a;
-#	getpack dev-libs/libgit2 libgit2 v0.17.0
-#fi
 
 export PATH=${CWD}:$PATH
 
