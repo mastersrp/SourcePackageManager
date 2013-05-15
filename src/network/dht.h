6f5744cdea091afaba42c9a5de687d5e16f43c3a
mastersrp <tcg.thegamer@gmail.com>
2013-05-15 16:14:32 +0200

    Add Tupfile for building the network parts. Add DHT library to main Tuprules linker line.

--- src/network/dht.h
+++ src/network/dht.h
@@ -1,4 +1,19 @@
 #ifndef SPM_NETWORK_DHT
 #define SPM_NETWORK_DHT
 
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
+#include <dht.h>
+
+#include "../fs.h"
+
 #endif /* SPM_NETWORK_DHT */
