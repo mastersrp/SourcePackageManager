db8d2461240de92fc6c59334d559cd954aaad307
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-05-17 15:33:41 +0000

    Add unstable support for calling system-installed git, if libgit2 is not implemented, for the following targets: 				clone 				pull

--- paklib/targets/clone.lua
+++ paklib/targets/clone.lua
@@ -3,6 +3,13 @@ target = {}
 target.name = "clone"
 
 target.execute = function( cfg )
+  if git == nil then -- if git is not builtin
+    -- revert to using system commands
+    git = {}
+    git.clone = function( repo, url )
+      return os.execute( "git clone " .. url .. " ./deps/" .. repo )
+    end
+  end
   for k,_ in pairs(cfg[1]) do
     if not io.exists( "./deps/" .. k .. "/.git/config" ) then
       print( "* Cloning \"" .. k .."\" into \"./deps/" .. k .. "\"..." )
--- paklib/targets/pull.lua
+++ paklib/targets/pull.lua
@@ -4,6 +4,16 @@ target.name = "pull"
 
 target.execute = function( cfg )
   goto eof
+  if git == nil then -- If git is not builtin
+    -- revert to using system commands
+    git = {}
+    git.pull = function( repo )
+      return os.execute( "cd ./deps/" .. repo .. "; git pull; cd ../..")
+    end
+    git.clone = function( repo, url )
+      return os.execute( "git clone " .. url .. " ./deps/" .. repo )
+    end
+  end
   local repos = {"github.com","bitbucket.com"}
   for d,_ in pairs(cfg[1]) do
     for k,v in pairs(repos) do
