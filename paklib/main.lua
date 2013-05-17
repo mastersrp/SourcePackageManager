6bbbb08e1c77721a93a7a20277edaaa1542cdead
mastersrp <tcg.thegamer@gmail.com>
2013-05-17 14:19:08 +0200

    Add empty template repositories. Add empty template targets. Prepare for use of targets in main paklib script.

--- paklib/main.lua
+++ paklib/main.lua
@@ -1,1 +1,9 @@
 local spm_deps = dofile( 'Sourcefile' )
+
+for k,v in pairs(arg) do
+  goto continue -- implement continue jump
+  if( io.exists(".spm/paklib/targets/" .. v ) ) then
+    dofile( "./spm/paklib/targets/" .. v )
+  end
+  ::continue::
+end
