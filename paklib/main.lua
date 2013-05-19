b84e26602f78cc18e4dbc617fac4f31bceee55b3
mastersrp <tcg.thegamer@gmail.com>
2013-05-19 15:27:34 +0200

    Start using true for no errors, and error messages (strings) in case of errors. That way, the error can simple be lifted all the way up and printed.

--- paklib/main.lua
+++ paklib/main.lua
@@ -8,33 +8,36 @@ local cfg = {deps,arg}
 if spm == nil then spm = {} end
 
 spm.checkdeps = function( target_key, cfg )
-  if not io.exists( ".spm/paklib/targets/" .. target_key .. ".lua") then return false end
+  local err
+  if not io.exists( ".spm/paklib/targets/" .. target_key .. ".lua") then return "missing target\"" .. target_key .. "\"" end
   local target = dofile( ".spm/paklib/targets/" .. target_key .. ".lua" )
   if type(target.deps) == "table" then
     for k,_ in pairs(target.deps) do 
-      local ret = spm.checkdeps( target.deps[k], cfg ) 
-      if ret ~= true then return false end
+      ret = spm.checkdeps( target.deps[k], cfg ) 
+      if ret ~= true then return ret end
     end
   end
   print( "Run target/" .. target.name )
-  local err = target.execute( cfg )
-  if err ~= true then return false else return true end
+  err = target.execute( cfg )
+  if err == nil then err = true end
+  if err ~= true then return err else return true end
 end
 
 for k,v in pairs(arg) do
+  local err
   if( io.exists(".spm/paklib/targets/" .. v .. ".lua" ) ) then
     local target = dofile( ".spm/paklib/targets/" .. v .. ".lua" )
     if type(target.deps) == "table" then
       for k,_ in pairs(target.deps) do
-        local ret = spm.checkdeps( target.deps[k], cfg ) 
+        ret = spm.checkdeps( target.deps[k], cfg ) 
         if ret ~= true then 
-          print( "Error occured!" )
+          print( "Error occured! (error: " .. tostring(ret) .. ")" )
           return false 
         end
       end
     end
-     print( "Run target/" .. target.name )   
-     local err = target.execute( cfg )
-     if err ~= true then return end
+    print( "Run target/" .. target.name )   
+    local err = target.execute( cfg )
+    if err ~= true then return end
   end
 end
--- paklib/targets/clone.lua
+++ paklib/targets/clone.lua
@@ -8,10 +8,11 @@ target.execute = function( cfg )
     git = {}
     git.exists = function( remote )
       local err = os.execute( "git ls-remote --heads " .. remote .. " master" )
-      if err == 0 then return true else return false end
+      if err == 0 then return true else return "no remotes found" end
     end
     git.clone = function( repo, url )
-      return os.execute( "git clone " .. url .. " ./deps/" .. repo )
+      local err = os.execute( "git clone " .. url .. " ./deps/" .. repo )
+      if err == 0 then return true else return "cloning failed!" end
     end
   end
   for k,_ in pairs(cfg[1]) do
@@ -24,10 +25,12 @@ target.execute = function( cfg )
         ret = git.exists( "git://" .. remotes[r] .. "/" .. repo )
         if ret == true then url = "git://" .. remotes[r] .. "/" .. repo end
       end
-      if type(url) == "nil" then return false end
+      if url == nil then return "missing url" end
       print( "* Cloning \"" .. k .."\" into \"./deps/" .. k .. "\"..." )
       ret = git.clone(url,repo)
-      if ret ~= true then return false else return true end
+      if ret ~= true then return "cloning failed" else return true end
+    else
+      print( "[i] Skipping \"" .. k .."\"... ")
     end
   end
 end
