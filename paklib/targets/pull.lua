056a5b789116acc38d262d6ea841ddbb836f00f3
mastersrp <tcg.thegamer@gmail.com>
2013-05-20 17:39:01 +0200

    Updated target 'pull' to have a more clear definition of how to pull.

--- paklib/targets/pull.lua
+++ paklib/targets/pull.lua
@@ -9,12 +9,18 @@ target.name = "pull"
 target.deps = {"clone"}
 
 target.execute = function( cfg )
+  goto eof
   local repos = {"github.com"}
   for d,_ in pairs(cfg[1]) do
     for k,v in pairs(repos) do
-      print( "looking up repo on '" .. v .. "/" .. deps[d]['url'] .. "'..." )
+      local repo = dofile '.spm/paklib/repo/' .. v .. '.lua'
+      local remote = string.gsub(repo.url,"%$(%w+)", cfg[1][d]["url"])
+      local err
+      err = git.exists( remote )
+      if err ~= true then return err end
     end
   end
+  ::eof::
 end
 
 return target
