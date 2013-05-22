009b0adf7be718737bc457e272e0e32ef309db72
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-05-22 12:42:45 +0000

    Add configure section to pkg["spm"] in Sourcefile, to allow for configuration of this specific package. Update target 'configure' to call each pkg[] .configure function.

--- paklib/targets/configure.lua
+++ paklib/targets/configure.lua
@@ -10,9 +10,13 @@ target.execute = function( cfg )
   for k,_ in pairs(cfg[1]) do
     local repo = cfg[1][k]["url"]
     print( "* Configuring \"" .. k .. "\" ... " )
-		conf.update( 'DIR_' .. string.upper(k) .. '_ROOT', './deps/' .. repo )
-		conf.update( 'USE_' .. string.upper(k), 'y' )
+    conf.update( 'DIR_' .. string.upper(k) .. '_ROOT', './deps/' .. repo )
+    conf.update( 'USE_' .. string.upper(k), 'y' )
     cfg[1][k].configure( "./deps/" .. repo )
+  end
+  for k,_ in pairs(cfg[3]) do
+    print( "*Configuring \"" .. k .. "\" ... " )
+    cfg[3][k].configure( cfg )
   end
 end
 
