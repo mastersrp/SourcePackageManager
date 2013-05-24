8c1fdcb079845437c548a698911c4529332f13a7
tcg.thegamer@gmail.com <tcg.thegamer@gmail.com>
2013-05-24 11:31:10 +0000

    Fix building in wrong order. Now runs any build functions BEFORE running tup.

--- paklib/targets/build.lua
+++ paklib/targets/build.lua
@@ -11,23 +11,25 @@ target.execute = function( cfg )
   print( "* Building everything..." )
   log.info( 'Building everything...' )
   local err
+  for k,_ in pairs(cfg["deps"]) do
+    if cfg["deps"][k].build ~= nil then
+      local repo = "./deps/" .. cfg["deps"][k]["url"]
+      err = cfg["deps"][k].build( repo )
+      if err ~= true then return "could not build " .. k end
+    end
+  end
+  for k,_ in pairs(cfg["pkg"]) do
+    if cfg["pkg"][k].build ~= nil then
+      err = cfg["pkg"][k].build(cfg)
+      if err ~= true then return "could not build " .. k end
+    end
+  end
   if io.exists('.tup/db') == false then
     err = sh 'tup init'
     if err ~= true then return "could not init tup database" end
   end
   err = sh 'tup upd'
   if err ~= true then return "could not build targets!" end
-  for k,_ in pairs(cfg["deps"]) do
-    if cfg["deps"][k].build ~= nil then
-      local repo = "./deps/" .. cfg["deps"][k]["url"]
-      cfg["deps"][k].build( repo )
-    end
-  end
-  for k,_ in pairs(cfg["pkg"]) do
-    if cfg["pkg"][k].build ~= nil then
-      cfg["pkg"][k].build(cfg)
-    end
-  end
   return true
 end
 
