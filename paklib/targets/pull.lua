4dfc411e7f6b4e82dcc63f3ccc1df278415beff7
mastersrp <tcg.thegamer@gmail.com>
2013-05-25 15:44:34 +0200

    Update target 'pull'. Now fully functional.

--- paklib/targets/pull.lua
+++ paklib/targets/pull.lua
@@ -4,6 +4,9 @@ if git == nil then
   require 'utils/git'
 end
 
+require 'utils/conf'
+require 'log'
+
 local paklib_root = os.getenv('PAKLIB_ROOT') or ".spm/paklib"
 
 target.name = "pull"
@@ -15,11 +18,13 @@ target.execute = function( cfg )
   for d,_ in pairs(cfg["deps"]) do
     for k,v in pairs(repos) do
       local repo = dofile( paklib_root .. '/repo/' .. v .. '.lua' )
-      local remote = string.gsub(repo.url,"%$(%w+)", cfg[1][d]["url"])
+      local remote = string.gsub(repo.url,"%$(%w+)", cfg["deps"][d]["url"])
       local err
+      print( "* Pulling in \"./deps/" .. cfg["deps"][d]["url"] .. "\"..." )
+      log.info( "* Pulling in \"./deps/" .. cfg["deps"][d]["url"] .. "\"..." )
       err = git.exists( remote )
-      if err ~= true then return err end
-      err = git.pull( "./deps/" .. cfg["deps"][k].url )
+      if err ~= true then return err end     
+      err = git.pull( "./deps/" .. cfg["deps"][d]["url"] )
       if err ~= true then return err end
     end
   end
